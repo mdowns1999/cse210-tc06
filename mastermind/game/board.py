b9591d85b22ee5a32d8b827763670a861e2180e0
mdowns <michaeljdowns99@gmail.com>
2021-05-26 11:13:30 -0600

    Snippts added

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -68,3 +68,39 @@ class Board:
         for n in range(piles):
             stones = random.randint(1, 9)
             self._piles.append(stones)
+
+
+
+    #CODE SNIPPTS DOWN BELOW
+    def prepare(self, player):
+        """Sets up the board with an entry for each player.
+        
+        Args:
+            self (Board): an instance of Board.
+        """
+        name = player.get_name()
+        code = str(random.randint(1000, 10000))
+        guess = "----"
+        hint = "****"
+        self._items[name] = [code, guess, hint]
+        
+    def _create_hint(self, code, guess):
+        """Generates a hint based on the given code and guess.
+
+        Args:
+            self (Board): An instance of Board.
+            code (string): The code to compare with.
+            guess (string): The guess that was made.
+
+        Returns:
+            string: A hint in the form [xxxx]
+        """ 
+        hint = ""
+        for index, letter in enumerate(guess):
+            if code[index] == letter:
+                hint += "x"
+            elif letter in code:
+                hint += "o"
+            else:
+                hint += "*"
+        return hint
