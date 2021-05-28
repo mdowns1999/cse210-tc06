96c1977a28490f4a77193ddaa5571e11e0abf154
mdowns <michaeljdowns99@gmail.com>
2021-05-28 16:11:47 -0600

    Added test Code

--- mastermind/game/test_cody1.py
+++ mastermind/game/test_cody1.py
@@ -1,14 +1,15 @@
 import random
-items = {}
+names = {}
 
-    
+
 name = 'Tim'
+name2="bill"
 code = str(random.randint(1000, 10000))
 guess = "----"
 hint = "****"
-items[name] = [code, guess, hint]
+names[name] = [code, guess, hint]
         
-print(items[name])
+#print(names[name])
 
 text =  "\n--------------------"
     #for pile, stones in enumerate(self._piles):
@@ -16,17 +17,49 @@ text =  "\n--------------------"
 text += "\n--------------------"
 
 fruit = {
-    "banana": 1.00,
-    "apple": 1.53,
-    "kiwi": 2.00,
-    "avocado": 3.23,
-    "mango": 2.33,
-    "pineapple": 1.44,
-    "strawberries": 1.95,
-    "melon": 2.34,
-    "grapes": 0.98
+    "banana": '1234',
+    "apple": '1234',
+    "kiwi": '1234',
+    "avocado": '1234',
+    "mango": '1234',
+    "pineapple": '1234',
+    "strawberries": '1234',
+    "melon": '1234',
+    "grapes": '1234'
 }
 
-for key, value in items.items():
-    value = " , ".join(value)
-    print("Player {}: {}".format(key, value))
+#for key, value in names.items():
+    #value = " , ".join(value)
+    #print("Player {}: {}".format(key, value))
+
+#Splits the value
+def split(variable):
+    return list(variable)
+guess = split(guess)
+print(guess)
+code = split(code)
+print(code)
+hint = split(hint)
+
+n = str(input('Enter Number: '))
+#guess = ["_"]*4
+#correct = split(correct)
+#print(correct)
+for i in range(0, 4): 
+  
+    # checking for equality of digits
+    if n[i] == code[i]:  
+
+        # hence, the digit is stored in correct[].
+        guess[i] = n[i]  
+
+
+print(guess)
+
+print( " ".join(guess))
+
+print(" ".join(code))
+
+print(" ".join(hint))
+
+
